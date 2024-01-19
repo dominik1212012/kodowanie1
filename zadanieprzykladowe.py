9ba9c2b56edb93e2b245fb1651541a5cbc7fe6e0
dominik1212012 <147029310+dominik1212012@users.noreply.github.com>
2024-01-19 12:46:03 +0100

    Delete zadanieprzykladowe.py

--- zadanieprzykladowe.py
+++
@@ -1,9 +0,0 @@
-n = int(input("podaj ilosc liczb"))
-wypisz = []
-for i in range(n):
-    x = float(input(f"podaj a{i+1}"))
-    wypisz.append(x)
-    for y in wypisz[:2]:
-        print(y)
-print(wypisz[0])
-
